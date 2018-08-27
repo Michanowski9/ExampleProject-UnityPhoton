35e7ff870188f2e44ad3d95d708c1bfc903e075a
Michanowski9 <m.sojka99@gmail.com>
2018-08-27 10:48:13 +0200

    Creating Room
    added some scripts(Creating Room), imported pun, folders

---
+++ ProjectUnityPhoton/Assets/Scripts/CreateRoom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateRoom : MonoBehaviour {
+
+    [SerializeField]
+    private Text _roomName;
+    private Text RoomName
+    {
+        get { return _roomName; }
+    }
+
+    public void OnClick_CreateRoom()
+    {
+        if (PhotonNetwork.CreateRoom(RoomName.text))
+        {
+            print("Creating room successfully sent.");
+        }
+        else
+        {
+            print("Creating room failed to send.");
+        }
+    }
+    private void OnPhotonCreateRoomFailed(object[] codeAndMessage)
+    {
+        print("Create Room Failed: " + codeAndMessage[1]);
+    }
+    private void OnCreatedRoom()
+    {
+        print("Room created successfully.");
+    }
+}
---
+++ ProjectUnityPhoton/Assets/Scripts/LobbyNetwork.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyNetwork : MonoBehaviour {
+
+    const string VERSION = "v0.1";
+
+	void Start () {
+        print("Connecting to server...");
+        PhotonNetwork.ConnectUsingSettings(VERSION);
+	}
+    void OnConnectedToMaster()
+    {
+        print("Connected to Master.");
+        PhotonNetwork.playerName = PlayerNetwork.Instance.PlayerName;
+
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+    void OnJoinedLobby()
+    {
+        print("Joined Lobby");
+    }
+}
